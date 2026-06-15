6ab16a2656676eb5c66dc674aa3dff805a9479c6
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2026-06-15 15:05:52 +0530

    chore: add more tests for heap(min)

--- dsa/python/test/test_2026_heap.py
+++ dsa/python/test/test_2026_heap.py
@@ -46,7 +46,62 @@ class TestMinHeap:
             if left < h.size():
                 assert value <= h._data[left]
             if right < h.size():
-                assert value <= h._data[right]
+                value <= h._data[right]
+
+    def test_single_element(self):
+        h = heap.MinHeap()
+        h.insert(42)
+        assert h.extract_min() == 42
+        assert h.size() == 0
+
+    def test_duplicates(self):
+        h = heap.MinHeap()
+        for _ in range(3):
+            h.insert(5)
+        assert h.extract_min() == 5
+        assert h.extract_min() == 5
+        assert h.extract_min() == 5
+
+    def test_negative_values(self):
+        h = heap.MinHeap()
+        h.insert(-1)
+        h.insert(-3)
+        h.insert(-2)
+        assert h.extract_min() == -3
+        assert h.extract_min() == -2
+        assert h.extract_min() == -1
+
+    def test_sorted_input(self):
+        h = heap.MinHeap()
+        for value in [1, 2, 3, 4, 5]:
+            h.insert(value)
+        assert h.extract_min() == 1
+        assert h.extract_min() == 2
+        assert h.extract_min() == 3
+
+    def test_reverse_sorted_input(self):
+        h = heap.MinHeap()
+        for value in [5, 4, 3, 2, 1]:
+            h.insert(value)
+        assert h.extract_min() == 1
+        assert h.extract_min() == 2
+        assert h.extract_min() == 3
+
+    def test_insert_after_extract(self):
+        h = heap.MinHeap()
+        h.insert(3)
+        assert h.extract_min() == 3
+        h.insert(1)
+        assert h.peek() == 1
+
+    def test_extract_all(self):
+        h = heap.MinHeap()
+        for value in [3, 1, 2]:
+            h.insert(value)
+        assert h.extract_min() == 1
+        assert h.extract_min() == 2
+        assert h.extract_min() == 3
+        assert h.extract_min() is None
 
 
 @pytest.mark.xfail(reason="Not implemented yet", raises=NotImplementedError)
